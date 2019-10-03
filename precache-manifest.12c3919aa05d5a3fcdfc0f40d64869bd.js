a321ec6ce12b79810e477f702676c7e3f98594b8
OffTop1 <1quyoshbekm@gmail.com>
2019-10-03 20:42:11 +0300

    Updates

---
+++ precache-manifest.12c3919aa05d5a3fcdfc0f40d64869bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d85fa41693e622df30d5ee873030fa2",
+    "url": "/nba-app/index.html"
+  },
+  {
+    "revision": "02e7998bcba1f61df9dd",
+    "url": "/nba-app/static/css/main.466c651d.chunk.css"
+  },
+  {
+    "revision": "76452c36ce966d911363",
+    "url": "/nba-app/static/js/2.6bdd80e2.chunk.js"
+  },
+  {
+    "revision": "02e7998bcba1f61df9dd",
+    "url": "/nba-app/static/js/main.894ff674.chunk.js"
+  },
+  {
+    "revision": "7a1d5b78426c532fab26",
+    "url": "/nba-app/static/js/runtime~main.28f554f2.js"
+  }
+]);
